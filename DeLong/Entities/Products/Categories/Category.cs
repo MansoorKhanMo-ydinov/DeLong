176cdf1966bf0922ead292c3e0b19a7e77e920e7
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-10-12 21:53:51 +0500

    Add new entities

---
+++ DeLong/Entities/Products/Categories/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeLong.Entities.Products.Categories
+{
+    internal class Category
+    {
+    }
+}
