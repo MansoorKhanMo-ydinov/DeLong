30b5047f05c484d5e62231e2956c361c1e7410ea
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-10-19 11:35:34 +0500

    Assets add

--- DeLong/obj/Debug/net8.0-windows/DeLong.AssemblyInfo.cs
+++ DeLong/obj/Debug/net8.0-windows/DeLong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029dc347e35decdca544f6b1a7ee2f550b383807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd2cb51297ccb878936a1788766f92f73a5f1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
