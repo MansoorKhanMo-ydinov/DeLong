e47998a3502ac12c6c5f1899305f83fb30ded3df
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-10-23 10:46:36 +0500

    Database created

--- DeLong/DbContexts/AppdbContext.cs
+++ DeLong/DbContexts/AppdbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace DeLong.DbContexts;
+﻿using DeLong.Entities.Users;
+using Microsoft.EntityFrameworkCore;
 
-public class AppdbContext
+namespace DeLong.DbContexts;
+
+public class AppdbContext:DbContext
 {
+    public DbSet<User> Users { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // PostgreSQL ulanish satrini kiriting
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=delongdb;Username=postgres;Password=mansurjon1607");
+    }
 }
