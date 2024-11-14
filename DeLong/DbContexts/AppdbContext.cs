f361d10db7483ae4f01e05d96067037233a54527
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-11-14 15:35:12 +0500

    scsxs

--- DeLong/DbContexts/AppdbContext.cs
+++ DeLong/DbContexts/AppdbContext.cs
@@ -1,4 +1,7 @@
-﻿using DeLong.Entities.Products;
+﻿using DeLong.Entities.Incomes;
+using DeLong.Entities.Informs;
+using DeLong.Entities.Products;
+using DeLong.Entities.Roles;
 using DeLong.Entities.Users;
 using DeLong.Entities.Warehouses;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +13,9 @@ public class AppdbContext:DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Warehouse> Warehouses { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Inform> Informs { get; set; }
+    public DbSet<Kirim> Kirims { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // PostgreSQL ulanish satrini kiriting
