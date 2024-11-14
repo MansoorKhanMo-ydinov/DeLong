98b6709197793237cbb4db1400f8fb9dba1888ce
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-11-14 13:56:02 +0500

    idg

--- DeLong/obj/Debug/net8.0-windows/DeLong.AssemblyInfo.cs
+++ DeLong/obj/Debug/net8.0-windows/DeLong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d70fe2406851e1d5b5194567265d895344a43c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01736dd42d6277398ab09f14230c8984ea9f98b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeLong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
