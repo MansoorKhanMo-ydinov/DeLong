a782713efb9ec89d18934a25fe4a8b9dcec7dd76
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-11-16 09:12:55 +0500

    Good Morning

--- DeLong/Pages/Incomes/KirimPage.xaml.cs
+++ DeLong/Pages/Incomes/KirimPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using DeLong.Entities.Incomes;
 
 namespace DeLong.Pages.Incomes
 {
@@ -11,5 +13,30 @@ namespace DeLong.Pages.Incomes
         {
             InitializeComponent();
         }
+
+        private void ShowDetails_Click(object sender, RoutedEventArgs e)
+        {
+            // Show the detailed products inside Inform
+            DetailDataGrid.Visibility = Visibility.Visible;
+
+            // Populate DetailDataGrid with the InformDetails of the selected KirimEntry
+            var selectedItem = kirimDataGrid.SelectedItem as Kirim;
+            if (selectedItem != null)
+            {
+                DetailDataGrid.ItemsSource = selectedItem.Inform;
+            }
+        }
+
+        private void CalculateTotals()
+        {
+            // Calculate and set total values based on InformDetails in each KirimEntry
+            foreach (var entry in kirimDataGrid.ItemsSource as List<Kirim>)
+            {
+                entry.Jaminarxi = entry.Inform.Sum(x => x.SotibOlishNarxi).CompareTo(int.Parse(Name));
+                entry.JamiSoni = entry.Inform.Sum(x => x.Soni);
+            }
+        }
+
+
     }
 }
