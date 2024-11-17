e8f56bbcfe19fb5daff215a1a402113ebf5b3511
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-11-17 09:10:32 +0500

    ghfg

--- DeLong/obj/Debug/net8.0-windows/Windows/Products/AddProductWindow.g.i.cs
+++ DeLong/obj/Debug/net8.0-windows/Windows/Products/AddProductWindow.g.i.cs
@@ -95,13 +95,13 @@ namespace DeLong.Windows.Products {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DeLong;component/windows/products/addproductwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DeLong;V1.0.0.0;component/windows/products/addproductwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Windows\Products\AddProductWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace DeLong.Windows.Products {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
